0aa827dc67da462c7b57b64aeb2e00a35a7948ab
Adwaith R Rajesh <107364380+adwaithrrajesh@users.noreply.github.com>
2023-06-14 16:41:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function toggleDropdown() {
+  const dropdown = document.getElementById('dropdown');
+  dropdown.classList.toggle('hidden');
+}
+
+
+function toggleShopDropdown() {
+  const dropdown = document.getElementById('shopDropdown');
+  dropdown.classList.toggle('hidden');
+}
